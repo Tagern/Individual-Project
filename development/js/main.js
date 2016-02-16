f0bbf8ee1c1962203134767f16b86045d5f9aeb9
Jamie Millman <jamiemillman@Jamies-MacBook-Pro-2.local>
2016-02-16 19:54:13 +0000

    developed mobile homepage

---
+++ development/js/main.js
@@ -0,0 +1,12 @@
+var  mn = $(".navigation");
+    mns = "navigation-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
+
