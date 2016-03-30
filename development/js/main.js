fe01f06c670a5c804ec74909bbd1956bdf82aea5
Jamie Millman <jamiemillman1992@hotmail.co.uk>
2016-03-31 00:41:19 +0100

    about us page

--- development/js/main.js
+++ development/js/main.js
@@ -1,3 +1,4 @@
+//NAVIGATION HIDE
 var  mn = $(".navigation");
     mns = "navigation-scrolled";
     hdr = $('header').height();
@@ -9,3 +10,42 @@ $(window).scroll(function() {
     mn.removeClass(mns);
   }
 });
+
+// TOGGLE DISPLAY
+function toggle_visibility() {
+   var e = document.getElementById('all-values');
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
+
+//DOCUMENT ONLOAD FOR SLIDE LEFT ELEMENT
+$(document).ready(function() {
+    if( $(window).width() > 1100 ) {
+    	$("#moving-cog").click(function() {
+    		document.getElementById("moving-cog").style.marginLeft = "-80px";
+    	});
+    } 
+    else {
+    	$("#moving-cog").click(null);
+
+    }
+    $("#moving-cog").click(toggle_visibility);
+});
+
+//RESIZING FOR SLIDE LEFT ELEMENT
+$(window).resize(function() {
+    if( $(window).width() > 1100 ) {
+    	$("#moving-cog").click(function() {
+    		document.getElementById("moving-cog").style.marginLeft = "-80px";
+    	});
+    } else {
+    	$("#moving-cog").unbind('click');
+    	document.getElementById("moving-cog").style.marginLeft = "";
+    }
+    $("#moving-cog").click(toggle_visibility);
+});
+
+
+
